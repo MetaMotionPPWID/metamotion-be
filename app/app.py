82bf41764c2152235701480a187086b84a86fd7e
Jakub Budzilo <84582825+Striker471@users.noreply.github.com>
2025-03-26 21:22:23 +0100

    Add register endpoint and required parameters validation

--- app/app.py
+++ app/app.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 import bcrypt
 from flask import Flask, request, jsonify
 from model import db
@@ -27,6 +25,54 @@ with app.app_context():
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    required_params = {
+        "login": str,
+        "password": str
+    }
+    errors = validation(data, required_params)
+    if errors:
+        return jsonify({"error": errors}), 400
+
+    login = request.json.get("login")
+    password = request.json.get("password")
+
+    if len(login) < 7 or len(login) > 60:
+        return jsonify({"error": "Login must be between 7 and 60 characters"}), 400
+    if len(password) < 7:
+        return jsonify({"error": "Password must have at least 7 characters"}), 400
+
+    existing_user = User.query.filter_by(login=login).first()
+    if existing_user:
+        return jsonify({"error": "User already exists"}), 400
+
+    hashed_password = bcrypt.hashpw(
+        password.encode('utf-8'),
+        bcrypt.gensalt()
+    ).decode('utf-8')
+
+    try:
+        new_user = User(login=login, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return '', 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+def validation(data, required_params):
+    errors = []
+    for param, expected_type in required_params.items():
+        if param not in data:
+            errors.append(f"Missing parameter: {param}")
+        elif not isinstance(data[param], expected_type):
+            errors.append(
+                f"Invalid type of parameter: {param}. Expected {expected_type}, received {type(data[param]).__name__}")
+    return errors
 
 
 if __name__ == '__main__':
